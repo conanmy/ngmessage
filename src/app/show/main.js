d9f617a556062bef88f3522daf00b07fb1545da2
conanmy <mayue@280F20DC35B844E.internal.baidu.com>
2015-05-04 20:50:16 +1200

    config ui router

--- src/app/show/main.js
+++ src/app/show/main.js
@@ -2,23 +2,22 @@
  * @file 消息展示模块主文件
  * @author conanmy
  */
-angular.module('show', ['ui.router'])
+angular.module('show', ['ui.router', 'showTab', 'showList'])
     .config(function($stateProvider) {
-        $stateProvider
-            .state('show', {
-                url: '/',
-                views: {
-                    '': {
-                        templateUrl: 'show/main.html'
-                    },
-                    'tab': {
-                        templateUrl: 'show/tab/main.html',
-                        controller: 'showTabCtrl'
-                    },
-                    'list': {
-                        templateUrl: 'show/list/main.html',
-                        controller: 'showListCtrl'
-                    }
+        $stateProvider.state('show', {
+            url: '/',
+            views: {
+                '': {
+                    templateUrl: 'app/show/main.html'
+                },
+                'tab@show': {
+                    templateUrl: 'app/show/tab/main.html',
+                    controller: 'showTabCtrl'
+                },
+                'list@show': {
+                    templateUrl: 'app/show/list/main.html',
+                    controller: 'showListCtrl'
                 }
-            });
+            }
+        });
     });
